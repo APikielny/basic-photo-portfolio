4518e62e0c098bfc195a564d615338377b7c27f2
APikielny <apikielny@gmail.com>
2022-08-16 15:34:20 -0700

    finish first batch of photos

--- components/photos.js
+++ components/photos.js
@@ -13,8 +13,8 @@ export const photos = [
   },
   {
     src: "/images/artistPointLandscape.jpg",
-    width: 3,
-    height: 4,
+    width: 4,
+    height: 3,
     tag: ["iPhone", "snow"]
   },
   {
@@ -31,6 +31,84 @@ export const photos = [
   },
   {
     src: "/images/flowers.jpg",
+    width: 4,
+    height: 3,
+    tag: ["iPhone"]
+  },
+  {
+    src: "/images/herman.jpg",
+    width: 3,
+    height: 4,
+    tag: ["iPhone", "snow"]
+  },
+  {
+    src: "/images/hermanCornice.jpg",
+    width: 3,
+    height: 4,
+    tag: ["iPhone", "snow"]
+  },
+  {
+    src: "/images/hermanSteep.jpg",
+    width: 3,
+    height: 4,
+    tag: ["iPhone", "snow"]
+  },
+  {
+    src: "/images/hoodoosSoftLight.jpg",
+    width: 4,
+    height: 3,
+    tag: ["iPhone", "rock"]
+  },
+  {
+    src: "/images/monkeyRock.jpg",
+    width: 4,
+    height: 3,
+    tag: ["iPhone"]
+  },
+  {
+    src: "/images/nevadaOpenRoad.jpg",
+    width: 4,
+    height: 3,
+    tag: ["iPhone"]
+  },
+  {
+    src: "/images/rifugioSunrise.JPG",
+    width: 4,
+    height: 3,
+    tag: []
+  },
+  {
+    src: "/images/rifugioSunset.jpeg",
+    width: 4,
+    height: 3,
+    tag: []
+  },
+  {
+    src: "/images/tetons.png",
+    width: 4,
+    height: 3,
+    tag: []
+  },
+  {
+    src: "/images/tetonsSunset.jpg",
+    width: 4,
+    height: 3,
+    tag: []
+  },
+  {
+    src: "/images/tetonsSunsetMoon.jpg",
+    width: 4,
+    height: 3,
+    tag: []
+  },
+  {
+    src: "/images/valtournencheBackflip.jepg",
+    width: 4,
+    height: 3,
+    tag: ["iPhone"]
+  },
+  {
+    src: "/images/wideHoodoos.jpg",
     width: 4,
     height: 3,
     tag: ["iPhone"]
