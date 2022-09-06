91edef7dedabddf01d46b8f323fd9d3600e416cd
APikielny <apikielny@gmail.com>
2022-09-05 18:38:41 -0700

    add me pics to about

--- components/About/About.js
+++ components/About/About.js
@@ -3,18 +3,49 @@ import Image from "next/image";
 import styles from "../../styles/About.module.css";
 
 import profilePic from "../../public/about/about_pic.jpeg";
+import meAction1 from "../../public/about/valtournencheBackflip.jpeg";
+import meAction2 from "../../public/about/craterLakeBackflip.jpg";
+import meAction3 from "../../public/about/adamRopeCoil.jpg";
+
 
 export default function About() {
   return (
     <div className="container">
       <div className={styles.aboutContainer}>
         <div className={styles.imageWrapper}>
-          <Image
+          {/* <Image
             src={profilePic}
             alt="Personal of myself"
             // Change this to adjust for the image size
             width={1920}
             height={2560}
+            layout="responsive"
+            className={styles.photo}
+          /> */}
+          <Image
+            src={meAction1}
+            alt="Personal of myself"
+            // Change this to adjust for the image size
+            width={1920}
+            height={2560}
+            layout="responsive"
+            className={styles.photo}
+          />
+          <Image
+            src={meAction2}
+            alt="Personal of myself"
+            // Change this to adjust for the image size
+            width={1920}
+            height={2560}
+            layout="responsive"
+            className={styles.photo}
+          />
+          <Image
+            src={meAction3}
+            alt="Personal of myself"
+            // Change this to adjust for the image size
+            // width={1920}
+            // height={2560}
             layout="responsive"
             className={styles.photo}
           />
